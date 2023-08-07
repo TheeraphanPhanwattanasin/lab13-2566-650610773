78d1c9fdabcbdb34ea332cbbed644d54627677e0
b <b>
2023-08-07 15:50:10 +0700

    all completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ export const Navbar = () => {
       <div className="d-flex justify-content-center gap-5 fs-4">
         <Link href="/">Home</Link>
         <Link href="/movie">Movie</Link>
-        <Link href="search">Search</Link>
         <Link href="/contact">Contact</Link>
+        <Link href="/search">Search</Link>
       </div>
       <hr />
     </div>
